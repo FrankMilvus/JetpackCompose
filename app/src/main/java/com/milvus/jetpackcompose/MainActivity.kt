5adadec9cbbc4ce72c2ee6e80e86187e248d6837
FrankMilvus <frankmilvus@gmail.com>
2025-08-05 23:40:56 +0300

    Image + Inscription

--- app/src/main/java/com/milvus/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/milvus/jetpackcompose/MainActivity.kt
@@ -4,73 +4,102 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.annotation.Size
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredWidth
-import androidx.compose.foundation.layout.width
-import androidx.compose.material3.Scaffold
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.milvus.jetpackcompose.ui.theme.JetpackComposeTheme
-import java.time.format.TextStyle
+import androidx.compose.ui.text.TextStyle
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            Column(
-                modifier = Modifier
-                    .background(Color.Green)
-                    .fillMaxHeight(1f)
-                    .fillMaxWidth(1f)
-                    .padding(20.dp, 40.dp)
-                    .border(5.dp, Color.Black)
-                    .padding(5.dp)
-                    .border(10.dp, Color.Red)
-                    .padding(10.dp)
-                    .border(10.dp, Color.Blue)
-
-            ) {
-                Text(
-                    "hello",
-                    modifier = Modifier
-                        .padding(20.dp)
-                        .border(5.dp, Color.Black)
-                        .padding(15.dp),
-                    fontSize = 20.sp,
+            val painter = painterResource(id = R.drawable.img)
+            val description = "Robo"
+            val title = "Robo"
+            ImageCard(painter,description,title)
 
 
-                    )
-                Spacer(modifier = Modifier.height(40.dp))
-                Text(
-                    "hello Man",
-                    modifier = Modifier
-                        .padding(16.dp)
-                )
-            }
+
         }
     }
 }
 
+//@Composable
+//@Preview
+//fun ImageCardPreview() {
+//    val painter = painterResource(id = R.drawable.img)
+//    val description = "Robo"
+//    val title = "Robo"
 
+//}
+@Composable
+fun ImageCard(
+    painter: Painter,
+    contentDescription: String,
+    title: String,
+    modifier: Modifier = Modifier
+) {
+    Card(
+        modifier = modifier
+            .padding(16.dp)
+            .fillMaxHeight(0.5f)
+            .fillMaxWidth(0.5f),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 15.dp
+        )
+    ) {
+        Box(modifier = Modifier.fillMaxSize()) {
+            Image(
+                painter = painter,
+                contentDescription = contentDescription,
+                contentScale = ContentScale.Crop
+            )
 
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(
+                        Brush.verticalGradient(
+                            colors = listOf(
+                                Color.Transparent,
+                                Color.Black
+                            ),
+                            startY = 300f
+                        )
+                    )
+            )
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(12.dp),
+                contentAlignment = Alignment.BottomStart
+            ) {
+                Text(title, style = TextStyle(color = Color.Magenta, fontSize = 16.sp))
+            }
+        }
+    }
+
+}
 
