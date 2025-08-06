79289f9fb9914b6c4a54b2d7e09bd99d8113790c
FrankMilvus <frankmilvus@gmail.com>
2025-08-06 11:14:57 +0300

    remember

--- app/src/main/java/com/milvus/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/milvus/jetpackcompose/MainActivity.kt
@@ -7,22 +7,18 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.text.SpanStyle
-import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
-import androidx.compose.ui.text.style.TextDecoration
-import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
+import kotlin.random.Random
 
 
 class MainActivity : ComponentActivity() {
@@ -32,32 +28,52 @@ class MainActivity : ComponentActivity() {
 
         val fontFamily = FontFamily(Font(R.font.mozilla_headline_old))
         setContent {
+            Column(modifier = Modifier.fillMaxSize()) {
+                val color = remember {
+                    mutableStateOf(
+                        Color.Blue
+                    )
+                }
 
-            ColorBox(Modifier
-                .fillMaxSize()
-                .padding(20.dp))
+                ColorBox(
+                    Modifier
+                        .weight(1f)
+                        .fillMaxSize()
+                        .padding(20.dp)
+                ) {
+                    color.value = it
+                }
+                Box(
+                    modifier = Modifier
+                        .background(color.value)
+                        .weight(1f)
+                        .fillMaxSize()
+                )
+            }
+
         }
     }
 }
 
 @Composable
 fun ColorBox(
-    modifier: Modifier= Modifier
-){
-    val color= remember{mutableStateOf(
-        Color.Red)
-    }
+    modifier: Modifier = Modifier,
+    updateColor: (Color) -> Unit
+) {
 
 
-    Box(modifier=modifier
-        .background(color.value)
-        .clickable(){
-            color.value=Color(
-                red = kotlin.random.Random.nextFloat(),
-                green = kotlin.random.Random.nextFloat(),
-                blue = kotlin.random.Random.nextFloat(),
-                alpha = 1f
-            )
+    Box(
+        modifier = modifier
+            .background(Color.Red)
+            .clickable() {
+                updateColor(
+                    Color(
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        Random.nextFloat(),
+                        alpha = 1f
+                    )
+                )
 
-        })
+            })
 }
