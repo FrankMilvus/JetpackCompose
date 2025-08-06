d8ef02bf84b49eafe588269984f718d2bc6664b5
FrankMilvus <frankmilvus@gmail.com>
2025-08-06 10:31:56 +0300

    Decorations of text

--- app/src/main/java/com/milvus/jetpackcompose/MainActivity.kt
+++ app/src/main/java/com/milvus/jetpackcompose/MainActivity.kt
@@ -4,102 +4,65 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextDecoration
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.unit.sp
-import androidx.compose.ui.text.TextStyle
 
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        val fontFamily = FontFamily(Font(R.font.mozilla_headline_old))
         setContent {
-            val painter = painterResource(id = R.drawable.img)
-            val description = "Robo"
-            val title = "Robo"
-            ImageCard(painter,description,title)
-
-
-
-        }
-    }
-}
-
-//@Composable
-//@Preview
-//fun ImageCardPreview() {
-//    val painter = painterResource(id = R.drawable.img)
-//    val description = "Robo"
-//    val title = "Robo"
-
-//}
-@Composable
-fun ImageCard(
-    painter: Painter,
-    contentDescription: String,
-    title: String,
-    modifier: Modifier = Modifier
-) {
-    Card(
-        modifier = modifier
-            .padding(16.dp)
-            .fillMaxHeight(0.5f)
-            .fillMaxWidth(0.5f),
-        shape = RoundedCornerShape(15.dp),
-        elevation = CardDefaults.cardElevation(
-            defaultElevation = 15.dp
-        )
-    ) {
-        Box(modifier = Modifier.fillMaxSize()) {
-            Image(
-                painter = painter,
-                contentDescription = contentDescription,
-                contentScale = ContentScale.Crop
-            )
-
             Box(
                 modifier = Modifier
                     .fillMaxSize()
-                    .background(
-                        Brush.verticalGradient(
-                            colors = listOf(
-                                Color.Transparent,
-                                Color.Black
-                            ),
-                            startY = 300f
-                        )
-                    )
-            )
-            Box(
-                modifier = Modifier
-                    .fillMaxSize()
-                    .padding(12.dp),
-                contentAlignment = Alignment.BottomStart
+                    .background(Color.DarkGray)
             ) {
-                Text(title, style = TextStyle(color = Color.Magenta, fontSize = 16.sp))
+                Text(
+                    buildAnnotatedString {
+                        withStyle(
+                            SpanStyle(
+                                Color.Green,
+                                55.sp
+                            )
+                        ){
+                            append("J")
+                        }
+                        append("etpack")
+
+                        withStyle(
+                            SpanStyle(
+                                Color.Green,
+                                55.sp
+                            )
+                        ){
+                            append("C")
+                        }
+                        append("ompose")
+                    },
+
+                    color = Color.White,
+                    fontSize = 30.sp,
+                    fontFamily = fontFamily,
+                    textDecoration = TextDecoration.LineThrough
+                )
+
+
             }
         }
     }
-
 }
-
